3a559790ce781e9c3ca47a2492c1a9603537c43c
SuperStik <daviswood290@gmail.com>
2021-09-07 19:21:09 -0700

    Add player model editor and error messages

--- lua/weapons/gmod_tool/stools/modeledit.lua
+++ lua/weapons/gmod_tool/stools/modeledit.lua
@@ -11,21 +11,28 @@ else
 		},
 		{
 			name = "right"
-		}
+		},
+	{
+		name = "reload"
+	}
 	}
 
 	language.Add("tool.modeledit.name", "Model")
 	language.Add("tool.modeledit.desc", "Swap around any model you want!")
 	language.Add("tool.modeledit.left", "Set an entity\'s model")
 	language.Add("tool.modeledit.right", "Get an entity\'s model")
+	language.Add("tool.modeledit.reload", "Set player model to selected model")
 
 	net.Receive("ModelEditString", function()
 		local mdl = net.ReadString()
+		local mode = net.ReadUInt(2)
 
-		if net.ReadBool() then
+		if mode == 0 then
 			notification.AddLegacy("Set model to \"" .. mdl .. "\"", 0, 5)
-		else
+		elseif mode == 1 then
 			notification.AddLegacy("Got model \"" .. mdl .. "\"", 0, 5)
+		else
+			notification.AddLegacy("Set player model to \"" .. mdl .. "\"", 0, 5)
 		end
 
 		surface.PlaySound("ambient/water/drip" .. math.random(1, 4) .. ".wav")
@@ -36,16 +43,20 @@ function TOOL:LeftClick(tr)
 	local mdl = self:GetClientInfo("model")
 	local ent = tr.Entity
 
-	if util.IsValidModel(mdl) and IsValid(ent) then
-		if SERVER then
-			ent:SetModel(mdl)
-			net.Start("ModelEditString")
-			net.WriteString(mdl)
-			net.WriteBit(true)
-			net.Send(self:GetOwner())
-		end
+	if IsValid(ent) then
+		if util.IsValidModel(mdl) then
+			if SERVER then
+				ent:SetModel(mdl)
+				net.Start("ModelEditString")
+				net.WriteString(mdl)
+				net.WriteUInt(0, 2)
+				net.Send(self:GetOwner())
+			end
 
-		return true
+			return true
+		else
+			self:GetOwner():SendLua("notification.AddLegacy('No valid model or entity selected!',1,5)surface.PlaySound('buttons/button10.wav')")
+		end
 	end
 end
 
@@ -56,14 +67,35 @@ function TOOL:RightClick(tr)
 	if IsValid(ent) then
 		if SERVER then
 			local mdl = ent:GetModel()
-			if not util.IsValidModel(mdl) then return false end
+
+			if not util.IsValidModel(mdl) then
+				owner:SendLua("notification.AddLegacy('Selected model is not valid!',1,5)surface.PlaySound('buttons/button10.wav')")
+
+				return false
+			end
+
 			owner:ConCommand("modeledit_model " .. mdl)
 			net.Start("ModelEditString")
 			net.WriteString(mdl)
-			net.WriteBit(false)
+			net.WriteUInt(1, 2)
 			net.Send(owner)
 		end
 
 		return true
+	end
+end
+
+function TOOL:Reload()
+	local owner = self:GetOwner()
+	local mdl = self:GetClientInfo("model")
+
+	if IsValid(owner) and util.IsValidModel(mdl) and SERVER then
+		owner:SetModel(mdl)
+		net.Start("ModelEditString")
+		net.WriteString(mdl)
+		net.WriteUInt(2, 2)
+		net.Send(owner)
+	else
+		owner:SendLua("notification.AddLegacy('No valid model selected!',1,5)surface.PlaySound('buttons/button10.wav')")
 	end
 end
