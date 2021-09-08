173008667a8519995a11e7717fa8a4ef9b09e7d1
SuperStik <daviswood290@gmail.com>
2021-09-07 18:24:05 -0700

    Add message for getting and setting models

--- lua/weapons/gmod_tool/stools/modeledit.lua
+++ lua/weapons/gmod_tool/stools/modeledit.lua
@@ -2,6 +2,20 @@ TOOL.Category = "Construction"
 TOOL.Name = "Model"
 TOOL.ClientConVar["model"] = ""
 
+if SERVER then
+	util.AddNetworkString("ModelEditString")
+else
+	net.Receive("ModelEditString", function()
+		local mdl = net.ReadString()
+
+		if net.ReadBool() then
+			notification.AddLegacy("Set model to \"" .. mdl .. "\"", 0, 3)
+		else
+			notification.AddLegacy("Got model \"" .. mdl .. "\"", 0, 3)
+		end
+	end)
+end
+
 function TOOL:LeftClick(tr)
 	local mdl = self:GetClientInfo("model")
 	local ent = tr.Entity
@@ -9,6 +23,10 @@ function TOOL:LeftClick(tr)
 	if util.IsValidModel(mdl) and IsValid(ent) then
 		if SERVER then
 			ent:SetModel(mdl)
+			net.Start("ModelEditString")
+			net.WriteString(mdl)
+			net.WriteBit(true)
+			net.Send(self:GetOwner())
 		end
 
 		return true
@@ -21,7 +39,12 @@ function TOOL:RightClick(tr)
 
 	if IsValid(ent) then
 		if SERVER then
-			owner:ConCommand("modeledit_model " .. ent:GetModel())
+			local mdl = ent:GetModel()
+			owner:ConCommand("modeledit_model " .. mdl)
+			net.Start("ModelEditString")
+			net.WriteString(mdl)
+			net.WriteBit(false)
+			net.Send(owner)
 		end
 
 		return true
