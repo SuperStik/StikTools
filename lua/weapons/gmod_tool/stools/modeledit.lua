5bc6ad3d3f19c63fc59f1543c0ecd8ef537c8f39
SuperStik <daviswood290@gmail.com>
2021-09-07 18:02:14 -0700

    Add left and right click functions

--- lua/weapons/gmod_tool/stools/modeledit.lua
+++ lua/weapons/gmod_tool/stools/modeledit.lua
@@ -1,2 +1,29 @@
 TOOL.Category = "Construction"
 TOOL.Name = "Model"
+TOOL.ClientConVar["model"] = ""
+
+function TOOL:LeftClick(tr)
+	local mdl = self:GetClientInfo("model")
+	local ent = tr.Entity
+
+	if util.IsValidModel(mdl) and IsValid(ent) then
+		if SERVER then
+			ent:SetModel(mdl)
+		end
+
+		return true
+	end
+end
+
+function TOOL:RightClick(tr)
+	local owner = self:GetOwner()
+	local ent = tr.Entity
+
+	if IsValid(ent) then
+		if SERVER then
+			owner:ConCommand("modeledit_model " .. ent:GetModel())
+		end
+
+		return true
+	end
+end
